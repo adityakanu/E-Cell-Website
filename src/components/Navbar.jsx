5a77e359f35b0ea62889ad0eb5efe9f7600b3d01
Aditya Kanu <81138360+adityakanu@users.noreply.github.com>
2023-06-18 17:25:01 +0530

    added favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,37 @@
 export default function Navbar() {
   return (
     <section id="navbar" className="font-Poppins">
-      <div>{/* Ankit apna code add kardena */
-      <ul>
-        <li>
-            <Link to="/">Home</Link>
-        </li>
+      <div>
+        {
+          /* Ankit apna code add kardena */
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
 
-        <li>
-            <Link to="/">Events</Link>
-        </li>
+            <li>
+              <Link to="/">Events</Link>
+            </li>
 
-        <li>
-            <Link to="/">Gallery</Link>
-        </li>
+            <li>
+              <Link to="/">Gallery</Link>
+            </li>
 
-        <li>
-            <Link to="/">Our Team</Link>
-        </li>
+            <li>
+              <Link to="/">Our Team</Link>
+            </li>
 
-        <li>
-            <Link to="/">Testimonials</Link>
-        </li>
+            <li>
+              <Link to="/">Testimonials</Link>
+            </li>
 
-        <li>
-            <Link to="/">Contact Us</Link>
-        </li>
-      </ul>} Hello</div>
+            <li>
+              <Link to="/">Contact Us</Link>
+            </li>
+          </ul>
+        }{" "}
+        Hello
+      </div>
     </section>
   );
 }
