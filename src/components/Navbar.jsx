f89c6a91ab4cba477abfa9c57045d557b5e5711c
Vedant Chainani <vedantchainani1084@gmail.com>
2023-08-09 15:31:27 +0530

    add responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,63 +1,111 @@
-import ecellWhite from "../assets/Logo-White.png";
+import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+import ecellWhite from '../assets/Logo-White.png';
+
+const navLinks = [
+	{
+		name: 'Home',
+		href: '#home',
+	},
+	{
+		name: 'About Us',
+		href: '#about-us',
+	},
+	{
+		name: 'Initiatives',
+		href: '#initiatives',
+	},
+	{
+		name: 'Gallery',
+		href: '#gallery',
+	},
+	{
+		name: 'Reach out',
+		href: '#reach-out',
+	},
+];
 
 export default function Navbar() {
-    return (
-        <section
-            id="navbar"
-            className="text-sm font-light bg-transparent font-Poppins"
-        >
-            <div className="absolute z-20 flex items-center justify-between w-full px-5 pt-4">
-                <a href="/" className="flex items-center">
-                    <img src={ecellWhite} className="w-[4rem] ml-8" alt="" />
-                </a>
+	const [hamburgerOpen, setHamburgerOpen] = React.useState(false);
 
-                <ul className="flex items-center md:mr-8 space-x-9">
-                    <li>
-                        <a
-                            className="font-thin text-amber-400 hover:text-cyan-300"
-                            href="/"
-                        >
-                            Home
-                        </a>
-                    </li>
+	return (
+		<>
+			<AnimatePresence>
+				{hamburgerOpen && (
+					<motion.div
+						className='h-full z-[100000] absolute text-white  w-full bg-black'
+						initial={{ y: 80, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						transition={{ delay: 0.5 }}
+						exit={{
+							opacity: 0,
+							y: 90,
+							transition: {
+								ease: 'easeInOut',
+								delay: 0.8,
+							},
+						}}
+					>
+						<div className='relative flex flex-col items-center justify-center my-48'>
+							<div className='flex flex-col items-center gap-12 text-4xl font-medium text-[#FBBF24]'>
+								{navLinks.map((link, index) => (
+									<motion.div
+										key={index}
+										className='hover:underline decoration-2'
+										onClick={() => {
+											setHamburgerOpen(false);
+										}}
+										initial={{ y: 80, opacity: 0 }}
+										animate={{ y: 0, opacity: 1 }}
+										transition={{ delay: 0.8 - index * 0.1 }}
+										exit={{
+											opacity: 0,
+											y: 90,
+											transition: {
+												ease: 'easeInOut',
+												delay: 1 - index * 0.2,
+											},
+										}}
+									>
+										{link.name}
+									</motion.div>
+								))}
+							</div>
+						</div>
+					</motion.div>
+				)}
+			</AnimatePresence>
 
-                    <li>
-                        <a
-                            className="font-thin text-amber-400 hover:text-cyan-300"
-                            href="/"
-                        >
-                            About Us
-                        </a>
-                    </li>
+			<div className='flex flex-row justify-between text-[#FBBF24] !bg-transparent font-medium relative p-8'>
+				<div
+					className={`z-[100000] flex rounded-full min-w-[60px] min-h-[60px] md:hidden items-center justify-center ${
+						hamburgerOpen && 'bg-white'
+					}`}
+				>
+					<button onClick={() => setHamburgerOpen(!hamburgerOpen)}>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							viewBox='0 0 256 256'
+							fill={hamburgerOpen ? '#000000' : '#fff'}
+							width={36}
+						>
+							<path d='M228,128a12,12,0,0,1-12,12H40a12,12,0,0,1,0-24H216A12,12,0,0,1,228,128ZM40,76H216a12,12,0,0,0,0-24H40a12,12,0,0,0,0,24ZM216,180H40a12,12,0,0,0,0,24H216a12,12,0,0,0,0-24Z' />
+						</svg>
+					</button>
+				</div>
 
-                    <li>
-                        <a
-                            className="font-thin text-amber-400 hover:text-cyan-300"
-                            href="/"
-                        >
-                            Initiatives
-                        </a>
-                    </li>
-
-                    <li>
-                        <a
-                            className="font-thin text-amber-400 hover:text-cyan-300"
-                            href="/"
-                        >
-                            Gallery
-                        </a>
-                    </li>
-
-                    <li>
-                        <a
-                            className="font-thin text-amber-400 hover:text-cyan-300"
-                            href="/"
-                        >
-                            Reach Out
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </section>
-    );
+				<div className='max-w-[4rem]'>
+					<img src={ecellWhite} alt='E-Cell Logo White' />
+				</div>
+				<div className='flex-row items-center hidden gap-6 md:flex'>
+					{navLinks.map((link, index) => (
+						<a href={link.href} key={index}>
+							<div className='hover:underline decoration-2'>{link.name}</div>
+						</a>
+					))}
+				</div>
+			</div>
+		</>
+	);
 }
